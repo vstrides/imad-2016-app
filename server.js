2828136d5f99663b015893d835826c2b7ed638fc
Pervaiz Khan <vstrides@users.noreply.github.com>
2016-09-20 01:54:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+    res.send("Article one will be server here soon !");
+});
+
+app.get('/article-two', function (req, res) {
+    res.send("Article two will be server here soon !");
+});
+
+app.get('/article-three', function (req, res) {
+    res.send("Article three will be server here soon !");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
