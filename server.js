2851c6f68805d811b2aebf44c754307eb8490496
Pervaiz Khan <vstrides@users.noreply.github.com>
2016-09-26 22:24:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,6 +116,12 @@ app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[articleName]));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter += 1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
