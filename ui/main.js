e5d30eb8386fc398dcdf70ea39000ab382c42808
Pervaiz Khan <vstrides@users.noreply.github.com>
2016-09-26 23:18:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+var counter = 0;
+
+button.onClick = function() {
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpReaquest.DONE)
+    {
+        if(request.status === 200)
+        {
+            counter = request.responseText;
+            var span = document.getElementById('count');  
+            span.innerHTML = counter.toString();
+        }
+    }
+};
+
+request.open('GET', 'http://vstrides.imad.hasura-app.io/counter', true);
+request.send(null);
+};
